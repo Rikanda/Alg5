ca09a6dc56296fb0c995c95054912aef493dfe57
Natalya <soltk7@yandex.ru>
2021-09-13 00:18:48 +0300

    Homework 5

---
+++ Alg5/MyTree.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alg5
+{
+    public class MyTree // класс для дерева
+    {
+        private static List<int> Values = new List<int>(); // лист для хранения добавляемых значений дерева, используется для проверки их уникальности
+        public static List<TreeNode<int>> ListTree = new List<TreeNode<int>>();// список для хранения нод дерева, в котором будет выполняться поиск
+
+        public static TreeNode<int> Root = null;
+
+        public static TreeNode<int> Tree(int n) // построение сбалансированного дерева с n узлами и уникальными значениями
+        {
+
+            TreeNode<int> newNode = null;
+
+
+            if (n == 0)
+                return null;
+            else
+
+            {
+
+                var nl = n / 2;
+                var nr = n - nl - 1;
+                newNode = new TreeNode<int>();
+
+                int MyNumber = 0;
+                newNode.Value = 0;
+                while (newNode.Value == 0)
+                {
+                    MyNumber = new Random().Next(10, 20);
+                    if (!Values.Contains(MyNumber))
+                    {
+                        Values.Add(MyNumber);
+
+                        newNode.Value = MyNumber;
+
+
+                    }
+                }
+                ListTree.Add(newNode);
+                newNode.LeftChild = Tree(nl);
+
+                newNode.RightChild = Tree(nr);
+
+            }
+            Console.WriteLine("возвращаемое значение " + newNode.Value);
+
+
+            Root = newNode;
+            return newNode;
+
+        }
+
+        // вывод листа на печать
+
+        public static void PrintList()
+        {
+            // проверка заполнения дерева
+
+            for (int i = 0; i<ListTree.Count; i++)
+            {
+                Console.WriteLine(ListTree[i].Value);
+
+                if (ListTree[i].LeftChild != null)
+                {
+                    Console.WriteLine("left" + ListTree[i].LeftChild.Value);
+                }
+
+                if (ListTree[i].RightChild != null)
+                {
+                    Console.WriteLine("right" + ListTree[i].RightChild.Value);
+                }
+
+
+
+            }
+
+        }
+
+    }
+}
--- Alg5/Program.cs
+++ Alg5/Program.cs
@@ -6,9 +6,122 @@ namespace Alg5
 {
     class Program
     {
+
+
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello World!");
+            // строим дерево с заданным количеством узлов
+            MyTree.Tree(10);
+
+
+            MyTree.PrintList();// просмотр  дерева
+
+           
+            TreeNode<int> Root = MyTree.Root;
+            Console.WriteLine("root= " + MyTree.Root.Value); // корень дерева
+
+            int searchValue = 16; // искомое значение в дереве
+
+            // BFS поиск значения в ширину с помощью очереди
+            var searchNodeQ = SearchByQueue(Root, searchValue);
+
+
+            // DFS поиск значения в глубину с помощью стека
+            var searchNodeS = SearchByStack(Root, searchValue);
+        }
+
+        public static Queue<TreeNode<int>> Q = new Queue<TreeNode<int>>(); //очередь для поиска
+
+        public static Stack<TreeNode<int>> S = new Stack<TreeNode<int>>(); // стак для поиска
+
+        public static TreeNode<int> SearchByQueue(TreeNode<int> root, int searchValue)
+        {
+            Q.Enqueue(root); // добавили в очередь корень
+            foreach (TreeNode<int> i in MyTree.ListTree) // обходим весь список
+            {
+                if (Q is null)
+                {
+                    Console.WriteLine("Список пуст, значение не найдено");
+                    return null;
+                }
+                else
+                {
+                    var n = Q.Dequeue(); // берем элемент из очереди
+                    Console.WriteLine("test= " + n.Value);
+                    if (n.Value == searchValue) // проверяем совпадение значения
+                    {
+                        Console.WriteLine("Искомый элемент найден");
+                        return n;
+                    }
+                    else // если не совпало, добавляем в очередь дочерние элементы (с проверкой что они не null)
+                    {
+                        Console.WriteLine("delete from stack " + n.Value);
+                        if (n.RightChild != null)
+
+                        {
+                            Q.Enqueue(n.RightChild);
+                            Console.WriteLine("add to stack " + n.RightChild.Value);
+                        }
+
+                        if (n.LeftChild != null)
+                        {
+                            Q.Enqueue(n.LeftChild);
+                            Console.WriteLine("add to stack " + n.LeftChild.Value);
+                        }
+
+                    }
+                }
+            }
+            return null; // если ничего не нашли
+        }
+
+        
+        public static TreeNode<int> SearchByStack(TreeNode<int> root, int searchValue ) // возвращает элемент по значению методом глубокого поиска
+        {
+            
+            S.Push(root); // добавили в стак корень
+            
+            foreach (TreeNode<int> i in MyTree.ListTree) // обходим весь список
+            {
+
+
+                if (S is null)
+                {
+                    Console.WriteLine("Список пуст, значение не найдено");
+                    return null;
+                }
+                else
+                {
+                    var n = S.Pop(); // берем элемент стака
+                    Console.WriteLine("test= "+ n.Value);
+                    if (n.Value == searchValue) // проверяем совпадение значения
+                    {
+                        Console.WriteLine("Искомый элемент найден");
+                        return n;
+                    }
+                    else // если не совпало, добавляем в стак дочерние элементы (с проверкой что они не null)
+                    {
+                        Console.WriteLine("delete from stack " + n.Value);
+                        if (n.RightChild != null)
+
+                        {
+                            S.Push(n.RightChild);
+                            Console.WriteLine("add to stack " + n.RightChild.Value);
+                        }
+
+                        if (n.LeftChild != null)
+                        {
+                            S.Push(n.LeftChild);
+                            Console.WriteLine("add to stack " + n.LeftChild.Value);
+                        }
+                    }
+                }
+            
+            }
+
+
+            return null; // если ничего не нашли
         }
     }
 }
